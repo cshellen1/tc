4e5c80abc7020514ed123cc826b18223e999b462
Chris Shellenberger <cshellen1@gmail.com>
2024-06-15 16:48:11 -0600

    prepared app for deployment

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -16,6 +17,7 @@ cloudinary.config({
 });
 
 const app = express();
+const __dirname = path.resolve();
 
 app.use(express.json({limit:"5mb"})); // to parse req.body
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
@@ -24,5 +26,22 @@ app.use(cookieParser()); // to parse cookies
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+
+/**
+ * Serves the frontend application in production mode.
+ *
+ * If the application is running in production mode (NODE_ENV === 'production'),
+ * this middleware will serve the frontend application's static files from the
+ * `/frontend/dist` directory. It will also handle all requests by sending the
+ * `index.html` file from the `/frontend/dist` directory, allowing the frontend
+ * application to handle routing.
+ */
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 export default app;
